cc6546020b406d658ff7c377c696a96a1729ac45
Rodney Ditchfield <rodney.ditch@hotmail.com>
2021-03-02 14:41:16 +1100

    routes configured sending text now

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
 const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => res.json({ msg: 'Welcome to the jungle' }));
+
+//Routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+//Port
+const PORT = process.env.port || 5000;
+
+app.listen(PORT, () => console.log(`Server started on ${PORT}`));
