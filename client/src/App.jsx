54ff07f7c1901c3a4970998b3915ce84d888a5cd
Rodney Ditchfield <rodney.ditch@hotmail.com>
2021-03-09 16:09:58 +1100

    alert functionality implemented, may remove or simply handle wih the backend at a later date

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,45 @@
 import React, { Fragment } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-//Contexts/ State
+//Contexts / States
 import ContactState from './context/contact/ContactState';
+import AuthState from './context/auth/AuthState';
+import AlertState from './context/alert/AlertState';
 
 //Layout
 import { Navbar } from './components/layout/Navbar';
+import { Alerts } from './components/layout/Alerts';
 
 //Pages
 import { Home } from './components/pages/Home';
 import { About } from './components/pages/About';
 
 // Components
+import { Register } from './components/auth/Register';
+import { Login } from './components/auth/Login';
+
 function App() {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className='container'>
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/about' component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <AlertState>
+          <Router>
+            <Fragment>
+              <Navbar />
+              <div className='container'>
+                <Alerts />
+                <Switch>
+                  <Route exact path='/' component={Home} />
+                  <Route exact path='/about' component={About} />
+                  <Route exact path='/register' component={Register} />
+                  <Route exact path='/login' component={Login} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
+        </AlertState>
+      </ContactState>
+    </AuthState>
   );
 }
 
