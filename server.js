6cf618ef6e04f6ff378ac0e1c52cdea1f425cf52
Rodney Ditchfield <rodney.ditch@hotmail.com>
2021-03-10 18:11:27 +1100

    added mongoose to server file

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const connectDB = require('./config/db');
+// const connectDB = require('./config/db');
+const mongoose = require('mongoose');
+
 require('dotenv').config();
 const path = require('path');
 const app = express();
