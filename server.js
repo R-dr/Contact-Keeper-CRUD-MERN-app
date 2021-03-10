bcec6c45fcb3ac4bdc8b34647314e409b92a8e72
Rodney Ditchfield <rodney.ditch@hotmail.com>
2021-03-10 17:19:27 +1100

    deploy round two... forgot to set staic assests...

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+require('dotenv').config();
 
 const app = express();
 
@@ -15,6 +16,15 @@ app.get('/', (req, res) => res.json({ msg: 'Welcome to the jungle' }));
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
+  app.get('*', (request, response) => {
+    response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 //Port
 const PORT = process.env.port || 5000;
